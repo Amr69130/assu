a036c3a3dae23f5df0d8b047a1825e5607e733a6
Amr69130 <amrouchenorris@gmail.com>
2025-03-24 16:34:19 +0100

    PRE MERGE

--- src/Model/Insurance.php
+++ src/Model/Insurance.php
@@ -13,4 +13,32 @@ class Insurance
         $this->name = $name;
 
     }
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
 }
