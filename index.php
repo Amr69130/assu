430d4beb5b298ea28246970175870c208f7aa814
Amr69130 <amrouchenorris@gmail.com>
2025-03-24 11:33:38 +0100

    add composer & namespaces

--- index.php
+++ index.php
@@ -1,25 +1,9 @@
 <?php
-// require_once("Manager/CarManager.php");
-// require_once("Model/Car.php");
-
-// $carManager = new CarManager();
-// $cars = $carManager->selectAllCars();
-
 $title = "Bienvenue dans le comparateur des assurances";
 require_once("header.php");
 ?>
 <h1 class="text-center">Listes des Assurances</h1>
-<div class="d-flex flex-wrap justify-content-evenly">
-    <!-- <?php foreach ($cars as $car): ?>
-        <div class="col-4 d-flex p-3 justify-content-center">
-            <img src="images/<?= $car->getImage() ?>" alt="<?= $car->getModel() ?>" style="height: 200px; width: auto;">
-            <div class="p-2">
-                <h2><?= $car->getModel() ?></h2>
-                <p><?= $car->getBrand() ?>, <?= $car->getHorsePower() ?> chevaux</p>
-            </div>
-        </div>
-    <?php endforeach; ?> -->
-</div>
+
 <?php
 require_once("footer.php");
 ?>
